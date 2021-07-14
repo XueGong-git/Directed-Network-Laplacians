f13d5c672731d56822b695e3d047d8514426dbd9
OpalGX <43104091+OpalGX@users.noreply.github.com>
2021-07-14 17:31:01 +0800

    Skip self loops

--- multilevel_likelihood.m
+++ multilevel_likelihood.m
@@ -1,5 +1,5 @@
 function [Ln] = multilevel_likelihood(h, G, gamma)
-% multilevel_likelihood  Calculate the likelihood of the Graph using the range dependent random
+% multilevel_likelihood  Calculate the likelihood that the graph came from the Trophic RDRG model
 % graph model
 %
 % INPUTS
@@ -17,15 +17,15 @@ A = adjacency(G);
 %loop through all i and j to update the log-likelihood
 for i = 1:Nnodes
     for j = 1:Nnodes
-        F_ij = (h(j)-h(i)-1)^2;% trophic incoherence of edge i->j
-        f_ij = 1/(1+exp(gamma*F_ij));% probability of edge i->j
+        I_ij = (h(j)-h(i)-1)^2;% trophic incoherence of edge i->j
+        f_ij = 1/(1+exp(gamma*I_ij));% probability of edge i->j
         lnf = log(f_ij);% log-likelihood of edge i->j
         ln0 = log(1-f_ij);% log-likelihood of absence of edge i->j
         
         %update log-likelihood of the graph by adding up the log-liklihood of edges
-        if A(i,j)==1
+        if A(i,j)==1 && i~=j %ignore self-loops
             Ln = Ln + lnf;
-        else
+        elseif i~=j
             Ln = Ln + ln0;
         end
         
