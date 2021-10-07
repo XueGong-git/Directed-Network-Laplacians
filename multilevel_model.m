759e1b511c58cc346144b6bfa9117a7218df7aa5
Xue Gong <43104091+OpalGX@users.noreply.github.com>
2021-10-07 13:48:20 +0800

    Update multilevel_model.m

--- multilevel_model.m
+++ multilevel_model.m
@@ -1,29 +1,41 @@
-function [W, h] = multilevel_model(k,n,gamma, a)
-N = n*k;
-h = sort(repmat(linspace(0,k-1,k),1,n)+(2*a*rand(1,N)-a));
-%create empty adjacency matrix
-W = zeros(N,N);
+function [A, h] = multilevel_model(K,m,gamma,a)
+% generateRDRG  Generate synthetic network from the trophic RDRG model
+% 
+% INPUTS
+% - K Number of clusters
+% - m Number of nodes per cluster
+% - gamma  Decay parameter
+% - a  Parameter for the additive noise
+%
+% OUTPUTS
+% - A Adjacency matrix of the synthetic network
+% - theta  True phase angles of nodes
 
-%create empty matrix for probability matrix
-f = zeros(N,N); %edge probability
-F = zeros(N,N); %incoherence
+n = m*K; % total number of nodes
+h = sort(repmat(linspace(0,K-1,K),1,m)+(2*a*rand(1,n)-a)); % trophic levels
+A = zeros(n,n); % preallocate adjacency matrix
+f = zeros(n,n); % preallocate edge probability
+F = zeros(n,n); % preallocate trophic incoherence
 
-for i = 1: (N-1)
-    for j = i+1 : N
-        F(i,j) = (h(j)-h(i)-1)^2; %incoherence of edge i->j
-        F(j,i) = (h(i)-h(j)-1)^2; %incoherence of edge j->i
-        f(i,j) = 1/(1+exp(gamma* F(i,j))); %probability of edge i->j
-        f(j,i) = 1/(1+exp(gamma* F(j,i))); %probability of edge j->i
+for i = 1: (n-1)
+    for j = i+1 : n
+        F(i,j) = (h(j)-h(i)-1)^2; % trophic incoherence of edge i->j
+        F(j,i) = (h(i)-h(j)-1)^2; % trophic incoherence of edge j->i
+        f(i,j) = 1/(1+exp(gamma* F(i,j))); % probability of edge i->j
+        f(j,i) = 1/(1+exp(gamma* F(j,i))); % probability of edge j->i
+        
+        %generate edge i->j with probaiblity f(i,j)
         if rand <= f(i,j)
-            W(i,j) = 1;
+            A(i,j) = 1;
         else
-            W(i,j) = 0;
+            A(i,j) = 0;
         end
         
+        %generate edge j->i with probaiblity f(j,i)
         if rand <= f(j,i)
-            W(j,i) = 1;
+            A(j,i) = 1;
         else
-            W(j,i) = 0;
+            A(j,i) = 0;
         end
     end
 end        
